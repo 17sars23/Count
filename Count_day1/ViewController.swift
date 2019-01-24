a35916b625c6e9644089314157ab03218152213e
17sars23 <17sars23@gmail.com>
2019-01-24 18:31:36 +0900

    Initial Commit

---
+++ Count_day1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Count_day1
+//
+//  Created by 川岸樹奈 on 2019/01/24.
+//  Copyright © 2019年 juna.Kawagishi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
